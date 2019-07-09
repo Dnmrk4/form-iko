8409f01f46f19d42e504881ca39796129cd6cb2c
Dnmrk4 <danmark.chemuren@gmail.com>
2019-07-09 15:50:02 +0300

    initial commit

---
+++ config.py
@@ -0,0 +1,17 @@
+
+class Config:
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'qwerty'
+
+class ProdConfig(Config):
+    pass
+
+class TestConfig(Config):
+    pass
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://john:1234@localhost/cheat'
+    DEBUG = True
+
+config_options ={"production":ProdConfig,"default":DevConfig,"testing":TestConfig}
+

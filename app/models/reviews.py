c23aa762e30e288d8e3930dde0c131d2ffe3c025
faithngotho <faithwangari248@gmail.com>
2019-07-11 10:59:02 +0300

    add added def class to save_reviews to the reviews.py

--- app/models/reviews.py
+++ app/models/reviews.py
@@ -13,5 +13,13 @@ def __init__(self,event_id,title,imageurl,review):
 def get_reviews(cls,id):
 
     responce = []
-    
+    for review in cls.all_reviews:
+        if review.event_id == id:
+            responce.append(review)
+            return responce
+
+
+def save_review(self):
+    Review.all_reviews.append(self) 
+
  

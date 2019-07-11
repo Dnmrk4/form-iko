9c96d8f011000b1bee30907bcd358a1280e47858
faithngotho <faithwangari248@gmail.com>
2019-07-11 10:54:18 +0300

    add added def class to the reviews.py

--- app/models/reviews.py
+++ app/models/reviews.py
@@ -3,4 +3,15 @@ Class Review:
  all_reviews = []
 
 def __init__(self,event_id,title,imageurl,review):
+    self.event_id = event_id
+    self.title = title
+    self.imageurl = imageurl
+    self.review = review
+
+
+@classmethod
+def get_reviews(cls,id):
+
+    responce = []
+    
  
